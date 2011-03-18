7d35521ab8e43f13ae98f632b06642c75546e730
Bryan O'Sullivan <bos@serpentine.com>
2011-03-17 22:21:45 -0700

    Add more benchmarks.

--- benchmarks/Benchmark.hs
+++ benchmarks/Benchmark.hs
@@ -1,25 +1,39 @@
+import Control.Exception
 import Control.Monad.ST
-
+import Criterion.Main
 import Data.Int
 import Data.Word
-
-import Criterion.Main
+import qualified System.Random as R
 import System.Random.MWC
+import qualified System.Random.Mersenne as M
 
 main = do
-  gen <- create
+  mwc <- create
+  mtg <- M.newMTGen . Just =<< uniform mwc
   defaultMain 
-    [ bench "mwc-Double" (uniform gen :: IO Double)
-    , bench "mwc-Int"    (uniform gen :: IO Int   )
-    , bench "mwc-Int8"   (uniform gen :: IO Int8  )
-    , bench "mwc-Int16"  (uniform gen :: IO Int16 )
-    , bench "mwc-Int32"  (uniform gen :: IO Int32 )
-    , bench "mwc-Int64"  (uniform gen :: IO Int64 )
-    , bench "mwc-Word"   (uniform gen :: IO Word  )
-    , bench "mwc-Word8"  (uniform gen :: IO Word8 )
-    , bench "mwc-Word16" (uniform gen :: IO Word16)
-    , bench "mwc-Word32" (uniform gen :: IO Word32)
-    , bench "mwc-Word64" (uniform gen :: IO Word64)
-    , bench "mwc-Integer" (uniform gen :: IO Word64)
-    , bench "normal"     (normal gen :: IO Double)
+    [ bgroup "mwc"
+      [ bench "Double"  (uniform mwc :: IO Double)
+      , bench "Int"     (uniform mwc :: IO Int)
+      , bench "Int8"    (uniform mwc :: IO Int8)
+      , bench "Int16"   (uniform mwc :: IO Int16)
+      , bench "Int32"   (uniform mwc :: IO Int32)
+      , bench "Int64"   (uniform mwc :: IO Int64)
+      , bench "Word"    (uniform mwc :: IO Word)
+      , bench "Word8"   (uniform mwc :: IO Word8)
+      , bench "Word16"  (uniform mwc :: IO Word16)
+      , bench "Word32"  (uniform mwc :: IO Word32)
+      , bench "Word64"  (uniform mwc :: IO Word64)
+      , bench "Integer" (uniform mwc :: IO Word64)
+      , bench "normal"  (normal mwc :: IO Double)
+      ]
+    , bgroup "random"
+      [
+        bench "Double" (R.randomIO >>= evaluate :: IO Double)
+      , bench "Int" (R.randomIO >>= evaluate :: IO Int)
+      ]
+    , bgroup "mersenne"
+      [
+        bench "Double" (M.random mtg :: IO Double)
+      , bench "Int" (M.random mtg :: IO Int)
+      ]
     ]
