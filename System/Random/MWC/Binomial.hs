908186f981d1a495cc4c173ada7b68d01c9010f2
Dominic Steinitz <dominic@steinitz.org>
2024-05-01 12:16:28 +0100

    Some experiments

--- System/Random/MWC/Binomial.hs
+++ System/Random/MWC/Binomial.hs
@@ -16,68 +16,10 @@ binomial :: StatefulGen g m
          -> Double            -- ^ Probability of success (returning True)
          -> g                 -- ^ Generator
          -> m Int
-binomial n p g =
-  if p == 0.0
-  then return 0
-  else if p == 1.0
-       then return n
-       else do
-    let q = 1 - p
-    if fromIntegral n * p < bInvThreshold
-      then do
-      let s = p / q
-      let a = fromIntegral (n + 1) * s
-      undefined
-      else undefined
+binomial n p g = undefined
 
--- FIXME: The paper contains mysterious (to me at least)
--- constants. The Rust author seems to understand these and makes
--- comments such as
---
---  * radius of triangle region, since height=1 also area of region
---
--- The Rust author also comments
---
---  * It is possible for BINV to get stuck, so we break if x >
--- BINV_MAX_X and try again.
---
--- * It would be safer to set BINV_MAX_X to self.n, but it is
--- extremely unlikely to be relevant.
---
--- * When n*p < 10, so is n*p*q which is the variance, so a result >
--- 110 would be 100 / sqrt(10) = 31 standard deviations away.
-
-
-baz :: StatefulGen g m => Int -> Double -> g -> m Int
-baz n p g = do
-  let q   = if p <= 0.5 then p else 1 - p
-      np  = fromIntegral n * p
-      npq = np * q
-      fm  = np + p
-      m   = floor fm
-      p1 = fromIntegral (floor (2.195 * sqrt npq - 4.6 * q)) + 0.5
-      -- FIXME: This comment I understand and will come back to
-      -- Tip of triangle
-      xm = fromIntegral m + 0.5
-      -- Left edge of triangle
-      xl = xm - p1
-      -- Right edge of triangle
-      xr = xm + p1
-      -- FIXME: I am not sure I understand this
-      -- p1 + area of parallelogram region
-      c  = 0.134 + 20.5 / (15.3 + fromIntegral m)
-      p2 = p1 * (1 + 2 * c)
-      lambda a = a * (1 + 0.5 * a)
-      lambdaL = lambda ((fm - xl) / (fm - xl * p))
-      lambdaR = lambda ((xr - fm) / (xr * q))
-      -- FIXME: p1 + area of left tail
-      p3 = p2 + c / lambdaL
-      -- p1 + area of right tail
-      p4 = p3 + c / lambdaR
-  return undefined
-
-bar :: StatefulGen g m => Int -> Double -> g -> m Int
-bar n p gen = do
+inv :: StatefulGen g m => Int -> Double -> g -> m Int
+inv n p gen = do
   let q = 1 - p
       s = p / q
       a = fromIntegral (n + 1) * s
@@ -90,34 +32,82 @@ bar n p gen = do
   u <- uniformDoublePositive01M gen
   let (_, _, x) = until (\(r, u, _) -> u <= r) f (r, u, 0) in return x
 
-foo :: StatefulGen g m => Int -> Double -> g -> m Int
-foo n p g = fmap sum $ fmap (fmap fromEnum) $ replicateM n $ bernoulli p g
+inv'' :: StatefulGen g m => Int -> Int -> Double -> g -> m [Int]
+inv'' m n p gen = do
+  let q = 1 - p
+      s = p / q
+      a = fromIntegral (n + 1) * s
+      r = q^n
+      f (rPrev, uPrev, xPrev) = (rNew, uNew, xNew)
+        where
+          uNew = uPrev - rPrev
+          xNew = xPrev + 1
+          rNew = rPrev * ((a / fromIntegral xNew) - s)
+  replicateM m $ do u <- uniformDoublePositive01M gen
+                    let (_, _, x) = until (\(r, u, _) -> u <= r) f (r, u, 0) in return x
+
+inv' :: StatefulGen g m => g -> ReaderT (Double, Double, Double) m Int
+inv' g = do
+  (s, a, r) <- ask
+  let f (rPrev, uPrev, xPrev) = (rNew, uNew, xNew)
+        where
+          uNew = uPrev - rPrev
+          xNew = xPrev + 1
+          rNew = rPrev * ((a / fromIntegral xNew) - s)
+  u <- lift $ uniformDoublePositive01M g
+  let (_, _, x) = until (\(r, u, _) -> u <= r) f (r, u, 0) in return x
+
+ber :: StatefulGen g m => Int -> Double -> g -> m Int
+ber n p g = fmap sum $ fmap (fmap fromEnum) $ replicateM n $ bernoulli p g
 
 nSamples = 100000
 
-test :: StatefulGen g m => g -> m (Double, Double)
-test g = do
-  ss <- replicateM nSamples $ bar 20 0.4 g
-  tt <- replicateM nSamples $ foo 20 0.4 g
-  return $ ((fromIntegral (sum ss) / fromIntegral nSamples), (fromIntegral (sum tt) / fromIntegral nSamples))
+testInv :: StatefulGen g m => g -> m Double
+testInv g = do
+  ss <- replicateM nSamples $ inv 1400 0.4 g
+  return $ fromIntegral (sum ss) / fromIntegral nSamples
 
-main :: IO ()
-main = do
+testInv'' :: StatefulGen g m => g -> m Double
+testInv'' g = do
+  ss <- inv'' nSamples 1400 0.4 g
+  return $ fromIntegral (sum ss) / fromIntegral nSamples
+
+testInv' :: StatefulGen g m => g -> ReaderT (Double, Double, Double) m Double
+testInv' g = do
+  ss <- replicateM nSamples $ inv' g
+  return $ fromIntegral (sum ss) / fromIntegral nSamples
+
+testBer :: StatefulGen g m => g -> m Double
+testBer g = do
+  tt <- replicateM nSamples $ ber 1400 0.4 g
+  return $ fromIntegral (sum tt) / fromIntegral nSamples
+
+mainB :: IO ()
+mainB = do
   monadicGen <- create
-  x <- runReaderT (ReaderT test) monadicGen
+  x <- runReaderT (ReaderT testBer) monadicGen
   print x
 
-  -- if uPrev > rPrev
+mainI :: IO ()
+mainI = do
+  monadicGen <- create
+  x <- runReaderT (ReaderT testInv) monadicGen
+  print x
 
-          -- if xPrev > bInvMaxX
-          -- then foo s a undefined
+mainII :: IO ()
+mainII = do
+  monadicGen <- create
+  x <- runReaderT (ReaderT testInv'') monadicGen
+  print x
 
-
--- Threshold for preferring the BINV algorithm / inverse cdf
--- logic. The paper suggests 10, Ranlib uses 30, R uses 30, Rust uses
--- 10 and GSL uses 14.
-bInvThreshold :: Double
-bInvThreshold = 10
-
-bInvMaxX :: Int
-bInvMaxX = 110
+mainMI :: IO ()
+mainMI = do
+  let n = 1400
+      p = 0.4
+      q = 1 - p
+      s = p / q
+      a = fromIntegral (n + 1) * s
+      r = q^n
+  g <- create
+  x <- runReaderT (ReaderT (runReaderT (testInv' g))) (s, a, r)
+  print x
--- System/Random/MWC/Distributions.hs
+++ System/Random/MWC/Distributions.hs
@@ -27,6 +27,8 @@ module System.Random.MWC.Distributions
     , geometric0
     , geometric1
     , bernoulli
+    , bar
+    , binomial
       -- ** Multivariate
     , dirichlet
       -- * Permutations
@@ -355,3 +357,122 @@ pkgError func msg = error $ "System.Random.MWC.Distributions." ++ func ++
 --   (2007). Gaussian random number generators.
 --   /ACM Computing Surveys/ 39(4).
 --   <http://www.cse.cuhk.edu.hk/~phwl/mt/public/archives/papers/grng_acmcs07.pdf>
+
+-- | Random variate generator for Binomial distribution
+--
+-- The probability of getting exactly k successes in n trials is
+-- given by the probability mass function:
+--
+-- \[
+-- f(k;n,p) = \Pr(X = k) = \binom n k  p^k(1-p)^{n-k}
+-- \]
+binomial :: forall g m . StatefulGen g m
+         => Int               -- ^ Number of trials
+         -> Double            -- ^ Probability of success (returning True)
+         -> g                 -- ^ Generator
+         -> m Int
+binomial n p g =
+  if p == 0.0
+  then return 0
+  else if p == 1.0
+       then return n
+       else do
+    let q = 1 - p
+    if fromIntegral n * p < bInvThreshold
+      then do
+      let s = p / q
+      let a = fromIntegral (n + 1) * s
+      bar n p g
+      else baz n p g
+
+-- FIXME: The paper contains mysterious (to me at least)
+-- constants. The Rust author seems to understand these and makes
+-- comments such as
+--
+--  * radius of triangle region, since height=1 also area of region
+--
+-- The Rust author also comments
+--
+--  * It is possible for BINV to get stuck, so we break if x >
+-- BINV_MAX_X and try again.
+--
+-- * It would be safer to set BINV_MAX_X to self.n, but it is
+-- extremely unlikely to be relevant.
+--
+-- * When n*p < 10, so is n*p*q which is the variance, so a result >
+-- 110 would be 100 / sqrt(10) = 31 standard deviations away.
+
+
+baz :: forall g m . StatefulGen g m => Int -> Double -> g -> m Int
+baz n p g = do
+  let q   = if p <= 0.5 then p else 1 - p
+      np  = fromIntegral n * p
+      npq = np * q
+      fm  = np + p
+      m   = floor fm
+      p1 = fromIntegral (floor (2.195 * sqrt npq - 4.6 * q)) + 0.5
+      -- FIXME: This comment I understand and will come back to
+      -- Tip of triangle
+      xm = fromIntegral m + 0.5
+      -- Left edge of triangle
+      xl = xm - p1
+      -- Right edge of triangle
+      xr = xm + p1
+      -- FIXME: I am not sure I understand this
+      -- p1 + area of parallelogram region
+      c  = 0.134 + 20.5 / (15.3 + fromIntegral m)
+      p2 = p1 * (1.0 + 2.0 * c)
+      lambda a = a * (1.0 + 0.5 * a)
+      lambdaL = lambda ((fm - xl) / (fm - xl * p))
+      lambdaR = lambda ((xr - fm) / (xr * q))
+      -- FIXME: p1 + area of left tail
+      p3 = p2 + c / lambdaL
+      -- p1 + area of right tail
+      p4 = p3 + c / lambdaR
+      f :: m Int
+      f = do
+        u <- uniformRM (0.0, p4) g
+        v <- uniformDoublePositive01M g
+        y <- if u <= p1
+             then return $ floor $ xm - p1 * v + u
+             else if u <= p2
+                  then undefined
+                  else undefined
+        return undefined
+                  -- then do let x = xl + (u - p1) / c
+                  --             v = v * c + 1.0 - abs (x - xm) / p1
+                  --         if v > 1
+                  --           then f
+                  --           else return $ floor x
+                  -- else return undefined
+  return undefined
+
+bar :: StatefulGen g m => Int -> Double -> g -> m Int
+bar n p gen = do
+  let q = 1 - p
+      s = p / q
+      a = fromIntegral (n + 1) * s
+      r = q^n
+      f (rPrev, uPrev, xPrev) = (rNew, uNew, xNew)
+        where
+          uNew = uPrev - rPrev
+          xNew = xPrev + 1
+          rNew = rPrev * ((a / fromIntegral xNew) - s)
+  u <- uniformDoublePositive01M gen
+  let (_, _, x) = until (\(r, u, _) -> u <= r) f (r, u, 0) in return x
+
+
+  -- if uPrev > rPrev
+
+          -- if xPrev > bInvMaxX
+          -- then foo s a undefined
+
+
+-- Threshold for preferring the BINV algorithm / inverse cdf
+-- logic. The paper suggests 10, Ranlib uses 30, R uses 30, Rust uses
+-- 10 and GSL uses 14.
+bInvThreshold :: Double
+bInvThreshold = 10
+
+bInvMaxX :: Int
+bInvMaxX = 110
